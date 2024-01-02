16a0fafdc46c4ab8802247248f70e00bb6efa0d1
Giliard H. da Silva <ghssilva248@gmail.com>
2024-01-02 19:52:44 -0300

    data-fetching

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import {Component} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component{
+  state = {
 
+    posts: []
+  }
+  timeoutUpdate = null 
+
+  componentDidMount(){
+    this.loadPosts()
+  }
+  
+  loadPosts = async () =>{
+    
+    const postResponse = fetch('https://jsonplaceholder.typicode.com/posts') /*aqui eu faço o requerimento dos dados */
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos') /*aqui eu faço o requerimento dos dados */
+    
+    const [photos, posts] = await Promise.all([photosResponse, postResponse ]) /*aqui crio um array com os dados */
+    
+    const postsJson = await posts.json() /*aqui eu converto para json os dados requeridos */
+    const photosJson = await photos.json() /*aqui eu converto para json os dados requeridos */
+
+    const photosAndPosts = postsJson.map((post, index) =>{
+      return {...post, cover: photosJson[index].url }
+    })/*aqui eu crio um novo array com o método map */
+
+    this.setState({ posts: photosAndPosts })
+  }
+
+  render(){
+    const {posts} = this.state
+
+    return(
+      <section className='container'>
+
+      <div className='posts'>
+        {posts.map((post) => (
+          <div className='post'>
+            <img src={post.cover} alt={post.title} />
+            <div key={post.id} className='post-card'>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div> 
+          </div>
+        
+        ))}
+      </div>
+      </section>
+    )
+  }
+}
 export default App;
